48fdde80e692af9e6d99ec4d232c2ff1cd668073
Jeff Lindsay <progrium@gmail.com>
2015-03-04 17:55:41 -0600

    added a blog, wrote first post. switched to Gunfile instead of Makefile as experiment

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem "middleman", "~>3.3.7"
 gem "middleman-deploy"
 gem "middleman-livereload"
 gem "middleman-syntax"
+gem "middleman-blog"
 
 # For feed.xml.builder
 gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -1,3 +1,13 @@
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+sprockets.append_path File.join root, 'bower_components'
+
+configure :development do
+  activate :livereload
+end
+
 activate :syntax
 
 activate :livereload
@@ -7,38 +17,33 @@ activate :deploy do |deploy|
   deploy.build_before = true
 end
 
-###
-# Page options, layouts, aliases and proxies
-###
+activate :blog do |blog|
+  blog.prefix = "blog"
+  blog.layout = "post"
+  blog.permalink = "{year}/{month}/{day}/{title}"
+  blog.sources = "{year}-{month}-{day}-{title}.html"
+
+  blog.year_link = "{year}.html"
+  blog.month_link = "{year}/{month}.html"
+  blog.day_link = "{year}/{month}/{day}.html"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page{num}"
+end
+
+page "/blog/atom.xml", layout: false
 
 # Per-page layout changes:
-#
-# With no layout
 # page "/path/to/file.html", :layout => false
-#
-# With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -46,29 +51,3 @@ end
 #     "Helping"
 #   end
 # end
-
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
-
-sprockets.append_path File.join root, 'bower_components'
-
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
-end
